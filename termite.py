38ea782ee00541bb40a5546854c0078541929246
Marcos Magallanes <marcos.m.magallanes@gmail.com>
2023-07-26 19:56:51 -0600

    TextLog update

--- termite.py
+++ termite.py
@@ -5,34 +5,28 @@ from textual.reactive import reactive
 from textual import log
 from textual.validation import ValidationResult, Validator
 
+from rich.console import Text
+
 from dotenv import load_dotenv
 load_dotenv()
 
 import openai
 
-class InputValidator(Validator):
-    """Validate input."""
-    def validate(self, value: str) -> ValidationResult:
-        def is_valid(value: str) -> bool:
-            return len(value) > 0
-        return self.success() if is_valid(value) else self.failure("No input!")
-
-class TermiteChatApplication(App):
+class Termite(App):
     """Main application."""
 
-    messages = reactive([{'role': 'system', 'content':'You are Termite, a terminal based AI assistant.'}])
+    CSS_PATH = 'styles.css'
+    messages = reactive([])
 
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
         yield TextLog(wrap=True, markup=True)
         yield Input(placeholder="Start typing...")
 
-    def on_ready(self) -> None:
+    def on_mount(self) -> None:
         """Handle application startup."""
-        conversation_log = self.query_one(TextLog)
 
-        for message in self.messages:
-            conversation_log.write(f'{message["role"]}: {message["content"]}')
+        self.add_message('system', 'You are Termite, a terminal based AI assistant.')
 
     def on_input_submitted(self, event: Input.Submitted) -> None:
         """Handle input submitted event."""
@@ -47,26 +41,41 @@ class TermiteChatApplication(App):
     def add_message(self, role, content):
         """Add to messages array and write to log."""
 
-        # Add to self.messages array
         entry = {'role': role, 'content': content}
-        self.messages.append(entry)
+        self.messages = [*self.messages, entry]
 
-        # Write to self.TextLog
-        conversation_log = self.query_one(TextLog)
-        conversation_log.write(f'{entry["role"]}: {entry["content"]}')
+    def watch_messages(self, old_messages: list, new_messages: list) -> None:
+        """Handle messages change by writing to log."""
+
+        diffs = [message for message in new_messages if message not in old_messages]
+
+        text_log = self.query_one(TextLog)
+        for message in diffs:
+            text_log.write(
+                Text(f'{message["role"]}: {message["content"]}')
+                )
+        
 
     async def send_request(self):
         """Sends a request to the OpenAI chat API."""
+
         messages = self.messages
-        log(f"Sending request with messages: {messages}")
+        # log(f"Sending request with messages: {messages}")
         response = await openai.ChatCompletion.acreate(
             model="gpt-3.5-turbo",
             messages=messages
         )
-        log(f"Received response: {response}")
+        # log(f"Received response: {response}")
         self.add_message('assistant', response.choices[0].message.content)
-        log(self.messages)
+        # log(self.messages)
+
+class InputValidator(Validator):
+    """Validate input."""
+    def validate(self, value: str) -> ValidationResult:
+        def is_valid(value: str) -> bool:
+            return len(value) > 0
+        return self.success() if is_valid(value) else self.failure("No input!")
 
 if __name__ == "__main__":
-    app = TermiteChatApplication()
+    app = Termite()
     app.run()
